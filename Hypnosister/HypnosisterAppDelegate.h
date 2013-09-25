405d5d5593712ba1ea6481cf9603ccaf55e397c5
Camron Schwoegler <camron@metafour.org>
2013-09-24 18:22:30 -0600

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Camron Schwoegler on 9/24/13.
+//  Copyright (c) 2013 Camron Schwoegler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Camron Schwoegler on 9/24/13.
+//  Copyright (c) 2013 Camron Schwoegler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnosisterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnosisterAppDelegate class]));
+    }
+}
